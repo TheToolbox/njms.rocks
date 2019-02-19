a78f7540ea12dde6afb7f796c4eb427ae6727c3e
Jason 'Toolbox' Oettinger <jason@oettinger.email>
2019-02-19 11:56:58 -0500

    Updating ssh-proxy port forwarding to 8080 because 80 isn't allowed

--- 3dprinting/set_up_octopi.sh
+++ 3dprinting/set_up_octopi.sh
@@ -18,7 +18,7 @@ sudo chmod 644 /lib/systemd/system/ssh-reverse-proxy.service
 # set up the command that the service calls, opening a Remote ssh proxy
 # NOTE: 
 echo "#!/bin/sh 
-ssh -v -i id_rsa ssh-user@njms.rocks -p 2222 -N -R 80:localhost:8080
+ssh -v -i id_rsa ssh-user@njms.rocks -p 2222 -N -R 8080:localhost:8080
 " > /opt/ssh-reverse-proxy.sh
 chmod +x /opt/ssh-reverse-proxy.sh
 
